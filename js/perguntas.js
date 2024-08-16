59c879126d89685924b1d618467a323331d10180
anagrzibovski <166126799+anagrzibovski@users.noreply.github.com>
2024-08-16 08:35:08 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,90 @@
+export const perguntas = [
+    {
+        enunciado:"Assim que saiu de casa você se depara com uma nova tecnologia",
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: [ "A tecnlogia é um problema nos dias de hoje," , 
+                "ela tem varias desvantagens"
+
+                ]
+            },
+            {
+                texto: "Isso é interresante",
+
+                afirmacao: [ "A tecnologia veio para auxiliar nosso dia a dia." ,
+                "a tecnologia veio para inovarmos cada dia mais" 
+            ]
+            }
+        ]
+    },
+    {
+        enunciado:" aplicar uma nova tecnologia de maneira eficaz requer uma abordagem de estratégica",
+        alternativas: [ "nos auxiliando em tudo que precisamos como nos estudos",
+            {
+                   texto: "É  uma capacidade que não trás benefícios nenhum para nós",
+                afirmacao: [ " ela prejudica o desempenho de atividades" ,
+                " ela permite que não aprendemos com facilidade" ,
+            
+            ]
+            
+            },
+            {
+                texto:" quais são os tres tipos de inteligencia artificial",
+                afirmacao:[
+                    "alem de ser uma forma mais facil de se mexer e interagir",
+                    "tudo se torna mais facil para aprendermos",
+                ]
+                                                   
+            }
+        ]
+    },
+    {
+        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: ["Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ]
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: ["Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            {
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: ["Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes.",
+                    "Ainda acha que os meios de desenho tradicionais são mais eficazes para a criatividade, por isso vem estimulando pessoas em suas redes sociais a fazer pintura em aquarela."
+                ]
+            },
+            {
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: ["Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!",
+                    "Compartilhou artes em redes sociais como forma de ensinar como se comunicar através da arte.",
+                    "Percebeu que muitas pessoas têm dificuldade em expressar suas ideias desenhando e acha que a IA é capaz de empoderar essas pessoas a tirarem ideias do papel."
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        alternativas: [
+            {
+                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+            },
+            {
+                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+            }
+        ]
+    },
+];
